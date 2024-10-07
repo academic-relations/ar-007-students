b38d8e9956d7e08d63911fe3bbccc2e6fa1a6f15
malloc3141 <malloc_3141592@naver.com>
2024-10-07 22:58:07 +0900

    fix: add back legacy colors

--- packages/web/src/styles/themes/colors.ts
+++ packages/web/src/styles/themes/colors.ts
@@ -27,6 +27,31 @@ const colors = {
     200: "#F5A3A8",
     700: "#B7202A",
   },
+  ORANGE: {
+    200: "#FDF7E6",
+    600: "#FF9211",
+  },
+  BLUE: {
+    200: "#E7F0FF",
+    600: "#065DAC",
+  },
+  PURPLE: {
+    200: "#F6EEFE",
+    600: "#9836EF",
+  },
+  PINK: {
+    200: "#FFF1F9",
+    600: "#DB2777",
+  },
+  YELLOW: {
+    200: "#FFFDE9",
+    600: "#E9A311",
+  },
+
+  MINT: {
+    300: "#92CCD4",
+    800: "#0C7F8E",
+  },
 };
 
 export default colors;
